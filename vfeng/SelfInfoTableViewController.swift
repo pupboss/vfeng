b678ecfacb0a8db956cb77558332dbf03eeb0bed
pupboss <pupboss.i@gmail.com>
2015-10-25 01:14:36 +0800

    SelfInfo finished

--- vfeng/SelfInfoTableViewController.swift
+++ vfeng/SelfInfoTableViewController.swift
@@ -7,85 +7,59 @@
 //
 
 import UIKit
+import Alamofire
 
 class SelfInfoTableViewController: BaseTableViewController {
 
+    var infoArr = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        getSelfInfoFromServer()
         
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "infocell")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func getSelfInfoFromServer() -> Void {
+        
+        MBProgressHUD.showMessage(Constants.Notification.LOADING)
+        
+        Alamofire.request(.GET, Constants.ROOT_URL + "account/" + Constants.UserInfo.USER_NAME).responseJSON { (response: Response<AnyObject, NSError>) -> Void in
+            
+            MBProgressHUD.hideHUD()
+            
+            if let arr = response.result.value as? Array<AnyObject> {
+                
+                self.infoArr = arr
+                
+                self.tableView.reloadData()
+            } else {
+                
+                MBProgressHUD.showError(Constants.Notification.NET_ERROR)
+            }
+        }
     }
 
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.infoArr.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("infocell", forIndexPath: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = self.infoArr[indexPath.row] as? String
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
